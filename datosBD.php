1a24d8b74bbad2488ffb2fd30a600af9d1a118ae
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-07-21 20:15:42 -0300

    Update datosBD.php
    Corrección de error de tipeo.

--- datosBD.php
+++ datosBD.php
@@ -8,6 +8,6 @@ $conn = new mysqli ($host, $usuario, $clave, $basededatos);
 mysqli_query ($conn , "SET character_set_result=utf8");
 if ($conn->connect_error)
 {
-    die ("Darabase Error: " . $conn->connect_error);
+    die ("Database Error: " . $conn->connect_error);
 } 
 ?>
