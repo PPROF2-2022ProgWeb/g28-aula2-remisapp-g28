4a34faaa21aa5143f56ec3952aa681b4f15146b7
Fabricio Matías Toso <88169799+FaMatias@users.noreply.github.com>
2022-08-06 02:20:50 -0300

    Repositorio de Sitio web en ingles.

---
+++ RemisApp-EN/datosBD.php
@@ -0,0 +1,13 @@
+<?php
+$host="localhost";
+$usuario="root";
+$clave="";
+$basededatos="remisapp";
+
+$conn = new mysqli ($host, $usuario, $clave, $basededatos);
+mysqli_query ($conn , "SET character_set_result=utf8");
+if ($conn->connect_error)
+{
+    die ("Database Error: " . $conn->connect_error);
+} 
+?>
---
+++ RemisApp-EN/js/funcionesGrupo6.js
@@ -0,0 +1,88 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav.clientHeight;
+    window.addEventListener('scroll', function() {
+        const currentTop = document.body.getBoundingClientRect().top * -1;
+        if ( currentTop < scrollPos) {
+        
+            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-visible');
+            } else {
+                console.log(123);
+                mainNav.classList.remove('is-visible', 'is-fixed');
+            }
+        } else {
+        
+            mainNav.classList.remove(['is-visible']);
+            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                mainNav.classList.add('is-fixed');
+            }
+        }
+        scrollPos = currentTop;
+    });
+})
+
+const edadLegal = 18;
+const esMayor = fechaNac => {
+  if(!fechaNac || isNaN(new Date(fechaNac))) return;
+  const hoy = new Date();
+  const dateNac = new Date(fechaNac);
+  if(hoy - dateNac < 0) return;
+  const years = hoy.getUTCFullYear() - dateNac.getUTCFullYear();
+  if(years < edadLegal) return false;
+  if(years > edadLegal) return true;
+  const meses = hoy.getUTCMonth() - dateNac.getUTCMonth();
+  if(meses < 0) return false;
+  if(meses > 0) return true;
+  const dias = hoy.getUTCDate() - dateNac.getUTCDate();
+  if(dias < 0) return false;
+  return true;
+}
+const edad = fechaNac => {
+  if(!fechaNac || isNaN(new Date(fechaNac))) return;
+  const hoy = new Date();
+  const dateNac = new Date(fechaNac);
+  if(hoy - dateNac < 0) return;
+  let dias = hoy.getUTCDate() - dateNac.getUTCDate();
+  let meses = hoy.getUTCMonth() - dateNac.getUTCMonth();
+  let years = hoy.getUTCFullYear() - dateNac.getUTCFullYear();
+  if(dias < 0) {
+    meses--;
+    dias = 30 + dias;
+  }
+  if(meses < 0) {
+    years--;
+    meses = 12 + meses;
+  }
+  
+  return [years, meses, dias];
+}
+
+$('#fechaNac').change(e => {
+  let mayor = esMayor(e.currentTarget.value);
+  let suEdad = edad(e.currentTarget.value);
+  if(mayor) {
+    $('#salida').text(`Usted es mayor de ${edadLegal} años`);
+  } else {
+    if(mayor === false) {
+    $('#salida').text(`Usted es menor de ${edadLegal} años`);
+    } else {
+      $('#salida').text('Fecha no válida, verifique');
+    }
+  }
+  if(suEdad) {
+    $('#edad').val(`${suEdad[0]} año(s), ${suEdad[1]} mes(es) y ${suEdad[2]} día(s).`);
+  } else {
+    $('#edad').val('');
+  }
+});
+
+function mOver(obj) {
+    obj.innerHTML = "¡Bienvenido!"
+  }
+  
+  function mOut(obj) {
+    obj.innerHTML = "¿A dónde vamos hoy?"
+  }
