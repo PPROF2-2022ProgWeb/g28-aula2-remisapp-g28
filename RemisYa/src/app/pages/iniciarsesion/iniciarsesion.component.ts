055c389853d29e30757011dd1d22a5143dd7dd4e
Fabricio Matias Toso <fabricio.toso@gmail.com>
2022-08-23 22:40:39 -0300

    Routing corrección

--- RemisYa/src/app/pages/iniciarsesion/iniciarsesion.component.ts
+++ RemisYa/src/app/pages/iniciarsesion/iniciarsesion.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IniciarsesionComponent implements OnInit {
 
+
+  loading: boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
