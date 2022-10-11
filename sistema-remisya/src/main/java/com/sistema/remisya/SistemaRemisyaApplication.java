0c0edce10727811a32b7e81e133946867c9b70cb
Fabricio Matias Toso <fabricio.toso@gmail.com>
2022-10-11 16:24:12 -0300

    Api_RY

---
+++ sistema-remisya/src/main/java/com/sistema/remisya/SistemaRemisyaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.remisya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRemisyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaRemisyaApplication.class, args);
+	}
+
+}
---
+++ sistema-remisya/src/test/java/com/sistema/remisya/SistemaRemisyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.remisya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRemisyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
