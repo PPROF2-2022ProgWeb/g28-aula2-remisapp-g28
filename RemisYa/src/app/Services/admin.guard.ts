bd8b7bb87f255855c1673cefff78d736ec28d69a
Fabricio Matias Toso <fabricio.toso@gmail.com>
2022-11-14 21:18:16 -0300

    .

---
+++ RemisYa/src/app/Services/admin.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+
+  constructor(private loginService:LoginService, private router:Router){
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.loginService.isLoggedIn() && this.loginService.getUserRol() == 'ADMIN'){
+        return true;
+      }
+    this.router.navigate(['LogIn']);
+    return true;
+  }
+
+}
